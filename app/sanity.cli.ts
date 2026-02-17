693882a66b89b00fd6f1a619c859e27da7f469f1
Shota Sepishvili <medicalgeorgialtd@gmail.com>
2026-02-18 02:16:26 +0400

    fix map error finally

---
+++ app/sanity.cli.ts
@@ -0,0 +1,11 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6ikh5d8u',
+    dataset: 'production'
+  },
+  deployment: {
+    appId: 'ptbt8zpkjyqqui8zowmt1jk9'
+  }
+})
---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '6ikh5d8u',
+    dataset: 'production'
+  }
+})
