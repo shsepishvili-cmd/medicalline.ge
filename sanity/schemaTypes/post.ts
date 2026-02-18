9b5d66df7cddacc121583fe4e10d9a8c2bdacf0e
Shota Sepishvili <medicalgeorgialtd@gmail.com>
2026-02-19 01:23:04 +0400

    fix: add export to post and product schemas

--- sanity/schemaTypes/post.ts
+++ sanity/schemaTypes/post.ts
@@ -1,1 +1,9 @@
-
+export default { // <--- აი ეს "export default" უნდა ეწეროს აუცილებლად!
+  name: 'post',
+  title: 'ბლოგი',
+  type: 'document',
+  fields: [
+    { name: 'title', title: 'სათაური', type: 'string' },
+    // ... დანარჩენი კოდი იგივეა
+  ]
+}
