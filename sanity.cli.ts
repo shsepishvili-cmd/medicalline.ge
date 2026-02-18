5927eb3d8eee0e431ab0be60d9af79d9aed47d75
Shota Sepishvili <medicalgeorgialtd@gmail.com>
2026-02-19 00:45:29 +0400

    fix: move post.ts and fix config path

--- sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import { defineCliConfig } from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '6ikh5d8u',
-    dataset: 'production'
-  }
-})
--- sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'next-sanity';
-
-export const client = createClient({
-  projectId: '6ikh5d8u',
-  dataset: 'production',
-  apiVersion: '2024-01-01',
-  useCdn: false, 
-});
