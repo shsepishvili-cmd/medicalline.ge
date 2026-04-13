415acc8591bddde27eade8bb2b4993fd735df039
Shota Sepishvili <medicalgeorgialtd@gmail.com>
2026-04-13 20:37:30 +0400

    fix: rs.ge sync, settings upsert logic and invoice reset

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,27 @@
 ﻿import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import Script from 'next/script'; 
 import GoogleTranslate from './GoogleTranslate';
-
-const inter = Inter({ subsets: ['latin'] });
+import AnalyticsBootstrap from './components/AnalyticsBootstrap';
+import { Suspense } from 'react';
+import {
+  CLARITY_ID,
+  GA_MEASUREMENT_ID,
+  GTM_ID,
+  META_PIXEL_ID,
+} from './lib/analytics';
+import { organizationSchema, siteConfig, websiteSchema } from './lib/seo';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://medicalline.ge'), 
+  metadataBase: new URL(siteConfig.url),
+  applicationName: siteConfig.name,
+  referrer: 'origin-when-cross-origin',
   title: {
-    default: 'Medical Line Georgia | Eighteeth-ის ექსკლუზიური დისტრიბუტორი',
-    template: '%s | Medical Line'
+    default: siteConfig.title,
+    template: '%s'
   },
-  description: 'Eighteeth-ის ოფიციალური წარმომადგენელი და წამყვანი სტომატოლოგიური ბრენდების (Hager, Philden...) პარტნიორი საქართველოში. შეიძინეთ აპარატურა და სავარძლები გარანტიით.',
-  keywords: ['სტომატოლოგიური აპარატურა', 'Eighteeth Georgia', 'Hager', 'Philden', 'ენდომოტორი', 'სტომატოლოგიური სავარძელი', 'Medical Line', 'ენდომოტორი', 'ინტრაორალური სკანერი', 'რენტგენი', 'სამედიცინო ტექნიკა', 'Medical Line'],
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
   icons: {
     icon: '/favicon.ico',
     shortcut: '/icon.png',
@@ -34,20 +42,26 @@ export const metadata: Metadata = {
     },
   },
   openGraph: {
-    title: 'Medical Line - ციფრული სტომატოლოგია',
-    description: 'Eighteeth-ის ოფიციალური წარმომადგენელი და სერვის ცენტრი საქართველოში. ყველაფერი თქვენი კლინიკისთვის.',
-    url: 'https://medicalline.ge',
-    siteName: 'Medical Line Georgia',
+    title: siteConfig.title,
+    description: siteConfig.description,
+    url: siteConfig.url,
+    siteName: siteConfig.name,
     images: [
       {
-        url: '/images/cover.png', 
+        url: siteConfig.ogImage,
         width: 1200,
         height: 630,
-        alt: 'Medical Line Georgia - Eighteeth Equipment',
+        alt: siteConfig.title,
       },
     ],
-    locale: 'ka_GE',
+    locale: siteConfig.locale,
     type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.title,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
   },
 };
 
@@ -55,53 +69,104 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ka" className="scroll-smooth">
       <head>
+        {GTM_ID ? (
+          <Script
+            id="gtm-base"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${GTM_ID}');
+              `
+            }}
+          />
+        ) : null}
+        {GA_MEASUREMENT_ID ? (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script
+              id="ga4-base"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  window.gtag = gtag;
+                  gtag('js', new Date());
+                  gtag('config', '${GA_MEASUREMENT_ID}', { send_page_view: false });
+                `
+              }}
+            />
+          </>
+        ) : null}
+        {CLARITY_ID ? (
+          <Script
+            id="clarity-base"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "${CLARITY_ID}");
+              `
+            }}
+          />
+        ) : null}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "MedicalBusiness",
-              "name": "Medical Line Georgia",
-              "image": "https://medicalline.ge/images/ml-logo.png",
-              "@id": "https://medicalline.ge",
-              "url": "https://medicalline.ge",
-              "telephone": "514011116",
-              "address": {
-                "@type": "PostalAddress",
-                "streetAddress": "დ. ჯაბიძის #8",
-                "addressLocality": "Tbilisi",
-                "addressCountry": "GE"
-              }
-            })
+            __html: JSON.stringify([organizationSchema(), websiteSchema()])
           }}
         />
       </head>
-      <body className={inter.className}>
-        
-        {/* 🚀 Meta Pixel Code (Safely Injected) */}
-        <Script id="fb-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '805898554494944');
-            fbq('track', 'PageView');
-          `
-        }} />
-        <noscript>
-          <img 
-            height="1" 
-            width="1" 
-            style={{ display: 'none' }} 
-            src="https://www.facebook.com/tr?id=805898554494944&ev=PageView&noscript=1" 
-            alt="" 
-          />
-        </noscript>
+      <body>
+        {GTM_ID ? (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
+        ) : null}
+
+        {META_PIXEL_ID ? (
+          <>
+            <Script id="fb-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${META_PIXEL_ID}');
+              `
+            }} />
+            <noscript>
+              <img 
+                height="1" 
+                width="1" 
+                style={{ display: 'none' }} 
+                src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+                alt="" 
+              />
+            </noscript>
+          </>
+        ) : null}
+
+        <Suspense fallback={null}><AnalyticsBootstrap /></Suspense>
 
         {children}
         
